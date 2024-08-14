7bfd975dfc7eada0e01dbf23a4763eb7c75ce74f
Kazu <hlyanw@yahoo.com>
2024-08-14 21:06:00 +0630

    Created new user making view

--- dj_react_webapp/backend/api/views.py
+++ dj_react_webapp/backend/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
+# A view to create a new user
+class CreateUserView(generics.CreateAPIView): # generics.CreateAPIView handles creating a new user or object
+    # Looking for all objects in User when create a new user and ensure is a new user
+    querset = User.objects.all()
+    # To accept valid data (username, password)
+    serializer_class = User
+    # Allowed anyone to use this view
+    permission_classes = [AllowAny]
