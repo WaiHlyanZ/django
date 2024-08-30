2cb2c92e58de2ef921d33090c90bb8dc90af8e8e
Kazu <hlyanw@yahoo.com>
2024-08-30 16:28:14 +0630

    Tested admin-site

--- django_basic/dj_tim_beginner/mysite/main/admin.py
+++ django_basic/dj_tim_beginner/mysite/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import ToDoList, Item
 # Register your models here.
+# Username = waihlyan
+# password = Zadmin@90807060504030
+
+# register to see on the admin website
+admin.site.register(ToDoList)
+admin.site.register(Item)
